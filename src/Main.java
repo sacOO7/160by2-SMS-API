b42c6e4f91f9cc4ad9a415cf5449c9de6c13d110
sachin <sachinshinde7676@gmail.com>
2016-04-09 15:01:42 +0530

    added doc

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) throws ResponseException, NotFound {
         M160by2 m160by2=new M160by2();
-        m160by2.login("7709758284","3139");
-        m160by2.sendSMS("hi shubham , happy gudhi padva","8983632885");
+        m160by2.login("","");
+        m160by2.sendSMS("","");
     }
 }
